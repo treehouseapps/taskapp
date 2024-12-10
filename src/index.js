85300165907696661a456c2df0d96e41f982f569
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-12-10 17:27:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './todo/app'
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(<app />)
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
